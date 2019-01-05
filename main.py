75187007f0cafdcd868a5cc6404ddf640b06ef57
humad <humad@live.com>
2019-01-04 21:20:40 -0500

    fixed bugs with reactions, added feature for time between messages

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from fbchat.models import *
 import string
 import getpass
 from emoji import UNICODE_EMOJI
+import sys
 
 class MessengerAnalysis:
     def __init__(self, email, password):
         self.client = Client(email, password)
         self.all_words = {}
         self.all_emojis = {}
+        self.all_reactions = {}
         self.users = {}
         self.words_by_users = {}
         self.emojis_by_users = {}
+        self.reactions_by_users = {}
+        self.longest_time = -sys.maxsize - 1
         self.messages = []
         self.words_to_skip = [
             'the',
@@ -174,6 +178,15 @@ class MessengerAnalysis:
         return cleaned_words
 
     def get_thread_list(self, limit=5):
+        self.all_words = {}
+        self.all_emojis = {}
+        self.all_reactions = {}
+        self.users = {}
+        self.words_by_users = {}
+        self.emojis_by_users = {}
+        self.reactions_by_users = {}
+        self.longest_time = -sys.maxsize - 1
+        self.messages = []
         return self.client.fetchThreadList(limit=limit)
 
     def get_user_name(self, user_id):
@@ -188,8 +201,33 @@ class MessengerAnalysis:
 
         self.messages = self.get_thread_messages(thread_id, limit)
         self.messages.reverse()
+        last_message_time = None
 
         for message in self.messages:
+
+            current_message_time = int(message.timestamp)
+            if last_message_time != None:
+                time_difference = current_message_time - last_message_time
+                if time_difference > self.longest_time:
+                    self.longest_time = time_difference
+            last_message_time = current_message_time
+
+            reactions = message.reactions
+            if reactions:
+                for id in reactions:
+                    reaction = reactions[id]
+                    if reaction not in self.all_reactions:
+                        self.all_reactions[reaction] = 0
+                    self.all_reactions[reaction] += 1
+
+                    if id not in self.reactions_by_users:
+                        self.reactions_by_users[id] = {}
+
+                    user_dict = self.reactions_by_users[id]
+                    
+                    if reaction not in user_dict:
+                        user_dict[reaction] = 0
+                    user_dict[reaction] += 1
 
             if not message.text:
                 continue
@@ -238,6 +276,15 @@ class MessengerAnalysis:
 
         print('Analysis Complete!')
 
+    def convert_millis_to_time(self):
+        millis = self.longest_time
+        seconds=(millis/1000)%60
+        seconds = int(seconds)
+        minutes=(millis/(1000*60))%60
+        minutes = int(minutes)
+        hours=(millis/(1000*60*60))
+        print ("%d:%d:%d" % (hours, minutes, seconds))
+
     def get_sorted_by_frequency(self, dict, reverse=True):
         return sorted(dict, key=dict.get, reverse=reverse)
 
@@ -251,11 +298,29 @@ class MessengerAnalysis:
             print(word, end=': ')
             print(dict[word])
 
+    def get_top_reactions(self, reverse=True):
+        sorted_by_frequency = self.get_sorted_by_frequency(self.all_reactions, reverse=reverse)
+        for reaction in sorted_by_frequency:
+            print(reaction.value, end=": ")
+            print(self.all_reactions[reaction])
+
+    def get_top_reactions_by_user(self, reverse=True):
+        for user_id in self.reactions_by_users:
+            reaction_dict = self.reactions_by_users[user_id]
+            user_name = self.users[user_id]
+            sorted_by_frequency = self.get_sorted_by_frequency(reaction_dict, reverse=reverse)
+            print(user_name)
+            for reaction in sorted_by_frequency:
+                print(reaction.value, end=": ")
+                print(reaction_dict[reaction])
 
     def get_top_words(self, limit=10, reverse=True):
         sorted_by_frequency = self.get_sorted_by_frequency(self.all_words, reverse=reverse)
         self.print_words(sorted_by_frequency, self.all_words, limit)
-            
+
+    def get_top_emojis(self, limit=10, reverse=True):
+        sorted_by_frequency = self.get_sorted_by_frequency(self.all_emojis, reverse=reverse)
+        self.print_words(sorted_by_frequency, self.all_emojis, limit=limit)
     
     def get_top_words_by_user(self, limit=10, reverse=True):
         for user_id in self.users:
@@ -273,6 +338,20 @@ class MessengerAnalysis:
             self.print_words(sorted_by_frequency, user_emojis_dict, limit)
             print('\n')
 
+def print_threads(threads):
+    for thread_id in threads:
+        print(thread_id)
+
+def print_options():
+    print("What would you like to do?")
+    print("[1] Get top words from conversation")
+    print("[2] Get top words per user")
+    print("[3] Get top emojis from conversation")
+    print("[4] Get top emojis per user")
+    print("[5] Get top reactions from conversation")
+    print("[6] Get top reactions per user")
+    print("[7] Get longest time between messages")
+    print("Type exit to quit")
 
 if __name__ == '__main__':
     print("Welcome to Messenger Analysis")
@@ -284,17 +363,40 @@ if __name__ == '__main__':
 
     ma = MessengerAnalysis(email, password)
 
-    print("Here are the 10 most recent messenger threads. Which one would you like to analyze?")
-    threads = ma.get_thread_list(10) 
+    print("Here are the 10 most recent messenger threads. Which one would you like to analyze?") 
 
-    for thread_id in threads:
-        print(thread_id)
+    while True:
+        threads = ma.get_thread_list(10)
+        print_threads(threads)
+        print("Insert thread ID (type exit to quit)", end=": ")
+        thread = input()
+        if thread == 'exit':
+            break
 
-    print("Insert thread ID", end=": ")
+        print("Analyzing messages...")
+        ma.get_messages_and_analyze(thread, 20000)
 
-    thread = input()
+        while True:
+            print_options()
+            option = input()
+            if option == 'exit':
+                break
+            if option == '1':
+                ma.get_top_words(10, True)
+            if option == '2':
+                ma.get_top_words_by_user(10, True)
+            if option == '3':
+                ma.get_top_emojis(5, True)
+            if option == '4':
+                ma.get_top_emojis_by_user(5, True)
+            if option == '5':
+                ma.get_top_reactions(True)
+            if option == '6':
+                ma.get_top_reactions_by_user(True)
+            if option == '7':
+                ma.convert_millis_to_time()
 
-    ma.get_messages_and_analyze(thread, 20000)
-    ma.get_top_words_by_user(10, True)
-    ma.get_top_emojis_by_user(5, True)
+    
+  
+    
 
